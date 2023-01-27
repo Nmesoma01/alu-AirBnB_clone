93d15d24dc578a18dfc2772b8c3fc1324fa1dd1a
eseosapku <e.uwagboe@alustudent.com>
2023-01-27 14:17:28 -0800

    commit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,4 +1,24 @@
 import unittest
+
+
+def add(x, y):
+    return x + y
+
 class TestCity(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
+    def fun_not_run(self):
+        print("no run")
+    def test_format(self):
+        # Tests that work for only a certain version of the library.
+        pass
+    def test_windows_support(self):
+        # windows specific testing code
+        pass
+    def test_add_two_positive_numbers(self):
+        result = add(2, 3)
+        self.assertEqual(result, 5)
+    def test_add_two_negative_numbers(self):
+        result = add(-2, -3)
+        self.assertEqual(result, -5)
+
