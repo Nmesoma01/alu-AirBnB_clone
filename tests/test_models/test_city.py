716e69887827ccf90bf0ffd701fa6d0eced36100
eseosapku <e.uwagboe@alustudent.com>
2023-01-27 13:36:50 -0800

    commit

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,3 +2,12 @@ import unittest
 class TestCity(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
+     def setUpClass(cls):
+        print("setUpClass")
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+    def test_fib_assert_equal(self):
+        self.assertEqual(fib(self.n), 55)
+    def test_fib_assert_true(self):
+        self.assertTrue(fib(self.n) == 55)
--- tests/test_models/test_file_storage.py
+++
@@ -1,4 +0,0 @@
-import unittest
-class TestCity(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
