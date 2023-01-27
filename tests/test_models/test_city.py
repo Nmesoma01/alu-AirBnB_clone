dcf0228c38bf730ae6e362f9ef00f14a2fcb81ac
eseosapku <e.uwagboe@alustudent.com>
2023-01-27 13:48:26 -0800

    comit

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,4 +4,11 @@ class TestCity(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
     def fun_not_run(self):
         print("no run")
-        
+    def test_nothing(self):
+        self.fail("shouldn't happen")   
+    def test_format(self):
+        # Tests that work for only a certain version of the library.
+        pass
+    def test_windows_support(self):
+        # windows specific testing code
+        pass
