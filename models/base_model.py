f720818b7b02c77c783f6ae6d372c477f8a53aba
eseosapku <e.uwagboe@alustudent.com>
2023-01-29 08:07:07 -0800

    comit

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self, id, created_at, updated_at):
+    self.id = id
+    self.created_at = datetime.now()
+    self.updated_at = datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        self.__dict__ =  
+    # code
+
