b9bd098645653f55f8449be15d370f40acf8a7dd
eseosapku <e.uwagboe@alustudent.com>
2023-02-01 06:20:10 -0800

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, id, created_at, updated_at):
-        self.id = str(uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        models.storage.new(self)
+        
 
     def save(self):
         self.updated_at = datetime.now()
