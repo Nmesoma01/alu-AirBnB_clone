eeb3fe0ee2f1a4143c0e1e987dd3f9d8ccdd4c7e
eseosapku <e.uwagboe@alustudent.com>
2023-02-01 07:16:34 -0800

    comit

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,6 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
     def save(self,*arg):
-
     def __dict__(self,*arg):
-
     def __str__(self,*arg):
