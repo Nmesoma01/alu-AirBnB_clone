7f24fefea4ee571291450fb23087a00aac8c2f48
eseosapku <e.uwagboe@alustudent.com>
2023-02-01 06:36:51 -0800

    commit

--- models/base_model.py
+++ models/base_model.py
@@ -3,18 +3,11 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, * **):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         
 
-    def save(self):
-        self.updated_at = datetime.now()
-        models.storage.save()
 
-    def to_dict(self):
-        self.__dict__ =  dict(created_at = self.created_at.isoformat())
-        self.__dict__ =  dict(updated_at = self.updated_at.isoformat())
-        self.__dict__ =  dict(created_at = self.created_at.isoformat())
 
