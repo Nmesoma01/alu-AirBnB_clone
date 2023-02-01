80ce04ffd95b9f17832349078434e0f7a45f2df8
eseosapku <e.uwagboe@alustudent.com>
2023-02-01 07:33:56 -0800

    comit

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,6 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    def my_fct(*arg, **kw):
-    print("{} - {}".format(arg, kw))
+    def __str__(*arg, **kw):
+        print("{} - {}".format(arg, kw))
  
